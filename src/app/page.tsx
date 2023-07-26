e9b4db8229a0775c95dc4bd9e672a93037598ed4
Filipe Leoni <filipe_sempre@outlook.com>
2023-07-26 19:57:26 -0300

    fix: deployment fetch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,6 @@
-// import { useQuery } from "react-query";
-// import { Heart, Lightning, Planet, Smiley } from "@phosphor-icons/react";
-
 import { useJikanAPI } from "@/hooks/useJikanApi";
 import SuperCarousel from "@/components/SuperCarousel";
 import GenreCarousel from "@/components/GenreCarousel";
-
-// import Spinner from "@/components/SpinnerLoading";
-// import { useAuth } from "@/context/AuthContext";
 
 export default async function Home() {
   const api = useJikanAPI();
@@ -37,120 +31,37 @@ export default async function Home() {
     return res.data;
   }
 
-  // console.log(dataExample);
-
-  // const actionData = await actionAnimes();
-
-  // const getTopAnimes = async () => {
-  //   const res = await api.getTopAnimes();
-  //   return res.data;
-  // };
-
-  // const getAnimesByGenre = async () => {
-  //   const res = await api.getAnimesByGenre(1);
-  //   return res.data;
-  // };
-  // const [topAnime, actionAnime] = await Promise.all([
-  //   getTopAnimes(),
-  //   getAnimesByGenre(),
-  // ]);
-  // const {
-  //   data: topAnimes,
-  //   isLoading: isLoadingTopAnimes,
-  //   error,
-  // } = useQuery("topAnimes", async () => {
-  //   const res = await api.getTopAnimes();
-  //   return res.data;
-  // });
-
-  // const fetchAnimesByGenre = async (genre: number) => {
-  //   const res = await api.getAnimesByGenre(genre);
-  //   return res.data;
-  // };
-
-  // const { data: actionAnimes, isLoading: isLoadingActionAnimes } = useQuery(
-  //   "actionAnimes",
-  //   () => fetchAnimesByGenre(1)
-  // );
-
-  // const { data: comedyAnimes, isLoading: isLoadingComedyAnimes } = useQuery(
-  //   "comedyAnimes",
-  //   () => fetchAnimesByGenre(4)
-  // );
-  // const { data: romanceAnimes, isLoading: isLoadingRomanceAnimes } = useQuery(
-  //   "romanceAnimes",
-  //   () => fetchAnimesByGenre(22)
-  // );
-  // const { data: isekaiAnimes, isLoading: isLoadingIsekaiAnimes } = useQuery(
-  //   "isekaiAnimes",
-  //   () => fetchAnimesByGenre(62)
-  // );
-
   return (
     <main>
       <title>AnimeZeta</title>
       <div className="grid grid-cols-1 w-full sm:grid-cols-4 md:grid-cols-6 lg:grid-cols-8 xl:grid-cols-12 gap-4 sm:gap-6 lg:max-w-5xl md:max-w-3xl mx-20 max-w-md sm:mx-0 pt-36 pb-20">
-        {/* {isLoadingTopAnimes ? (
-          <div className="flex justify-center items-center w-full col-span-12 h-64">
-            <Spinner />
-          </div>
-        ) : (
-        )} */}
         <SuperCarousel data={topAnime} />
 
         <GenreCarousel
           AnimeData={actionAnime}
           Title="Get Ready for Thrilling Action Anime!"
-          // Icon={Lightning}
         />
 
-        {/* {isLoadingActionAnimes ? (
-          <div className="flex justify-center items-center w-full col-span-12 h-64">
-            <Spinner />
-          </div>
-        ) : ( */}
         <GenreCarousel
           AnimeData={actionAnime}
           Title="Get Ready for Thrilling Action Anime!"
-          // Icon={Lightning}
         />
-        {/* )} */}
 
-        {/* {isLoadingComedyAnimes ? (
-          <div className="flex justify-center items-center w-full col-span-12 h-64">
-            <Spinner />
-          </div>
-        ) : ( */}
         <GenreCarousel
           AnimeData={comedyAnime}
           Title="Laugh Out Loud with Comedy Anime!"
-          // Icon={Smiley}
         />
-        {/* // )} */}
 
-        {/* {isLoadingRomanceAnimes ? (
-          <div className="flex justify-center items-center w-full col-span-12 h-64">
-            <Spinner />
-          </div>
-        ) : ( */}
         <GenreCarousel
           AnimeData={romanceAnime}
           Title="Delve into Romance Anime!"
-          // Icon={Heart}
         />
-        {/* )} */}
 
-        {/* {isLoadingIsekaiAnimes ? (
-          <div className="flex justify-center items-center w-full col-span-12 h-64">
-            <Spinner />
-          </div>
-        ) : ( */}
         {/* <GenreCarousel
             AnimeData={isekaiAnimes}
             Title="Dive into Otherworldly Adventures with Isekai Anime!"
             // Icon={Planet}
           /> */}
-        {/* )} */}
       </div>
     </main>
   );
--- src/services/fetch.ts
+++ src/services/fetch.ts
@@ -1,7 +1,10 @@
-export async function fetchWrapper(url: string) {
-  const data = await fetch(`${process.env.JIKAN_URL}${url}`);
+export async function fetchWrapper<T = unknown>(
+  input: RequestInfo | URL,
+  init?: RequestInit | undefined
+) {
+  const data = await fetch(`${process.env.JIKAN_URL}${input}`, init);
   const result = await data.json();
-  return result;
+  return result as T;
 }
 
 export async function fetchApi<T = unknown>(
